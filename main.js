d8a9651fe47e556583267ff7b65b72f838dd6571
Vsevolod <cerneavschivsevolod@gmail.com>
2021-03-04 17:28:32 +0100

    Esercizio finito con sort senza sort non riesco ad ordinare in modo alfabetico!

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
-
 // Chiedere all’utente il cognome
 // inserirlo in un array con altri cognomi,
 // stampa la lista ordinata alfabeticamente
 // scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
 
-var listaCognomi = ["Bianchi", "Rossi", "Perlari", "Lodigiani", "Mastrobattista", "Verdi"];
+var lista = ["Bianchi", "Rossi", "Perlari", "Lodigiani", "Mastrobattista", "Verdi"];
 
-var cognomeUtente = prompt("Dammi il tuo cognome: ");
-listaCognomi.push(cognomeUtente);
-listaCognomi.sort();
-console.log(listaCognomi);
+
+do {
+  var cognomeUtente = prompt("Dammi il tuo cognome: ");
+  var cognomeUtente = cognomeUtente[0].toUpperCase() + cognomeUtente.slice(1)
+} while (!isNaN(cognomeUtente))
+lista.push(cognomeUtente);
+lista.sort();
+console.log(cognomeUtente);
+
 
 console.log(cognomeUtente);
-for (i = 0; i < listaCognomi.length; i++) {
-  if(cognomeUtente === listaCognomi[i]) {
+for (i = 0; i < lista.length; i++) {
+  if (cognomeUtente === lista[i]) {
     console.log(i)
   }
 }
