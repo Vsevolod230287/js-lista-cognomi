919eb9c57d79ac7b47bd496c9e768cce2767969c
Vsevolod <cerneavschivsevolod@gmail.com>
2021-03-04 15:41:36 +0100

    Fatto anche Posizione Umana del Cognome inserito

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
+
 // Chiedere all’utente il cognome
-// inserirlo in un array con altri cognomi: ‘Bianchi’, ‘Rossi’, ‘Perlari’, ‘Lodigiani’, 'Mastrobattista', ‘Verdi’
+// inserirlo in un array con altri cognomi,
 // stampa la lista ordinata alfabeticamente
 // scrivi anche la posizione "umana" della lista in cui il nuovo utente si trova
-var listaCognomi = ["Bianchi", "Rossi", "Perlari", "Lodigiani", "Mastrobattista", "Verdi"];
-var cognomeUtente = prompt("Dammi il tuo cognome: ")
 
+var listaCognomi = ["Bianchi", "Rossi", "Perlari", "Lodigiani", "Mastrobattista", "Verdi"];
+
+var cognomeUtente = prompt("Dammi il tuo cognome: ");
 listaCognomi.push(cognomeUtente);
 listaCognomi.sort();
 console.log(listaCognomi);
+
+console.log(cognomeUtente);
+for (i = 0; i < listaCognomi.length; i++) {
+  if(cognomeUtente === listaCognomi[i]) {
+    console.log(i)
+  }
+}
